b7bce8a99c9eda483907b161f71711231e3b81f5
Reijii01 <147913247+Reijii01@users.noreply.github.com>
2025-06-08 11:26:42 +0300

    Добавил для другой группы.

--- config.py
+++ config.py
@@ -6,3 +6,8 @@ load_dotenv()
 TOKEN = os.getenv("TOKEN")
 DB_PATH = os.getenv("DB_PATH")
 GROUP_CHAT_ID = int(os.getenv("GROUP_CHAT_ID"))
+
+TOKEN2 = os.getenv("TOKEN2")
+GROUP_CHAT_ID_FOR_REGION = int(os.getenv("GROUP_CHAT_ID_FOR_REGION"))
+
+
